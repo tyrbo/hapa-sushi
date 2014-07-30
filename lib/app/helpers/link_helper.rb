f9d79aef869cfcc214e28a11909214657105edbd
Jonmichael Chambers <jon@tyrbo.net>
2014-07-30 14:12:21 -0600

    Implements Catering page

--- lib/app/helpers/link_helper.rb
+++ lib/app/helpers/link_helper.rb
@@ -1,7 +1,6 @@
 module LinkHelper
   def classes_for(path:, matcher: nil, children: false)
     matcher = matcher || path
-    puts matcher
     classes = ['menu-item', 'menu-item-type-custom', 'menu-item-object-custom']
     classes << 'menu-item-has-children' if children
     classes << 'current_page_item' if is_active_page?(matcher)
