9c0f22609c9582056b37ed01482a3d849e50251f
Jonmichael Chambers <jon@tyrbo.net>
2014-07-30 23:24:27 -0600

    Fixes broken class methods (for include).

--- lib/app/modules/path_builder.rb
+++ lib/app/modules/path_builder.rb
@@ -1,5 +1,9 @@
 module PathBuilder
-  class << self
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
     def create_with_path(params)
       obj = new(params)
       obj.path = params[:title].gsub(/[^0-9a-z ]/i, '').gsub(/[\s]/, '-').downcase
